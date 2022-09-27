e8141e82d9dba42c9512e451f58ea636034e651b
0maddox <nickkiim7@gmail.com>
2022-09-28 00:09:25 +0300

    completed assignment

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React , { useState } from "react";
 
-function CategoryFilter() {
-  return (
-    <div className="categories">
-      <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
-    </div>
-  );
+function CategoryFilter({ categories, handleFilter }) {
+
+	const [useSelected, setSelected] = useState("");
+	return (
+		<div className="categories">
+			<h5>Category filters</h5>
+			{/* render <button> elements for each category here */}
+			{categories.map((category, index) => {
+				return (
+					<button
+						key={index}
+						onClick={(e) => {
+							handleFilter(e.target.value);
+							e.target.className = "selected";
+							// handleState();
+						}}
+						value={category}
+						className=""
+					>
+						{category}
+					</button>
+				);
+			})}
+		</div>
+	);
 }
 
 export default CategoryFilter;
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import Task  from "./Task";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ tasks, currentTask, handleDelete }) {
+
+	const renderTasks = tasks
+		.filter((item) => {
+			if (currentTask === "All") {
+				return true;
+			} else {
+				return item.category === currentTask;
+			}
+		})
+		.map((task, index) => {
+			return (
+				<Task
+					key={index}
+					text={task.text}
+					category={task.category}
+					handleDelete={handleDelete}
+				/>
+			);
+		});
+
+	return (
+		<div className="tasks">
+			{/* display a list of tasks using Task component */}
+			{renderTasks}
+		</div>
+	);
 }
 
 export default TaskList;
